12907c388d2e18afd909cf64f1caf2ea5c053598
Hridoy Banik <hridoy.banik71@gmail.com>
2021-05-12 19:29:46 +0600

    changed test.get file

--- test_1/test_get.py
+++ test_1/test_get.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -7,4 +9,5 @@ def test_get():
     driver.get("https://bikroy.com/")
     driver.implicitly_wait(5)
     assert driver.title == "Bikroy.com - বাংলাদেশে ইলেকট্রনিকস, গাড়ি, প্রপার্টি এবং চাকরি"
+    time.sleep(3)
     driver.quit()
